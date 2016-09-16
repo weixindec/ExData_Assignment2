48628118b07e92c515b1f3c84dc9a528ebc879c5
weixindec <weixin_dec@yahoo.com>
2016-09-16 11:29:56 +0800

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,6 +1,6 @@
 ## This first line will likely take a few seconds. Be patient!
-#NEI <- readRDS("summarySCC_PM25.rds")
-#SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 #calculate sum of emission by year
 library(dplyr)
